4333cfbf90548745ffa9ca397f9505d96614690d
Krish Shah <2003kshah@gmail.com>
2022-01-04 08:26:34 -0500

    added mockup2 and mockup3

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -126,8 +126,7 @@ function Features() {
                   leaveEnd="opacity-0 -translate-y-16"
                 >
                   <div className="relative inline-flex flex-col">
-                    <img className="md:max-w-none mx-auto rounded" src={require('../images/features-bg.png').default} width="500" height="462" alt="Features bg" />
-                    <img className="md:max-w-none absolute w-full left-0 transform animate-float" src={require('../images/features-element.png').default} width="500" height="44" alt="Element" style={{ top: '30%' }} />
+                    <img className="md:max-w-none mx-auto rounded" src={require('../images/Mockup2.webp').default} width="300" height="600" alt="Features bg" />
                   </div>
                 </Transition>
                 {/* Item 3 */}
@@ -143,8 +142,7 @@ function Features() {
                   leaveEnd="opacity-0 -translate-y-16"
                 >
                   <div className="relative inline-flex flex-col">
-                    <img className="md:max-w-none mx-auto rounded" src={require('../images/features-bg.png').default} width="500" height="462" alt="Features bg" />
-                    <img className="md:max-w-none absolute w-full left-0 transform animate-float" src={require('../images/features-element.png').default} width="500" height="44" alt="Element" style={{ top: '30%' }} />
+                   <img className="md:max-w-none mx-auto rounded" src={require('../images/Mockup3.webp').default} width="300" height="600" alt="Features bg" />
                   </div>
                 </Transition>
               </div>
