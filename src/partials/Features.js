66f23d8d302ce2943a96efee33e729fef3681543
Krish Shah <2003kshah@gmail.com>
2022-01-03 21:32:34 -0500

    svg added

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -32,9 +32,9 @@ function Features() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">What We Do</h1>
           </div>
-
+          
           {/* Section content */}
-          <div className="md:grid md:grid-cols-12 md:gap-6">
+          {/*<div className="md:grid md:grid-cols-12 md:gap-6">*/}
 
             {/* Content */}
             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6" data-aos="fade-right">
